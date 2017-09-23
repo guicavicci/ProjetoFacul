3d0d2f2f96902081a3a5004342a798476d6f42d0
rodymuniz <ro.muniz.s@gmail.com>
2017-09-23 00:25:06 -0300

    Update Fatura.java

--- src/beans/Fatura.java
+++ src/beans/Fatura.java
@@ -2,95 +2,95 @@ package beans;
 
 public class Fatura
 {
-    private int numeroFatura;
-    private double consumoKwh;
-    private String dataVencimento;
-    private String formaPagamento;
-    private double valor;
-    private double taxaKwh;
-    
-    public Fatura(int numeroFatura, double consumoKwh, String dataVencimento, String formaPagamento, double valor, double taxaKwh)
-    {
-        setNumeroFatura(numeroFatura);
-        setConsumoKwh(consumoKwh);
-        setDataVencimento(dataVencimento);
-        setFormaPagamento(formaPagamento);
-        setValor(valor);
-        setTaxaKwh(taxaKwh);
-    }
-    
-    public Fatura(){}
-    
-    public String getAll()
-    {
-        return "Número Fatura: "+numeroFatura+
-                "\nConsumo (KW/h): "+consumoKwh+
-                "\nData de vencimento: "+dataVencimento+
-                "\nForma de Pagamento: "+formaPagamento+
-                "\nValor: R$ "+valor+
-                "\nTaxa (Reais por KW/h)"+taxaKwh;
-    }
-    public void setAll(int numeroFatura, double consumoKwh, String dataVencimento, String formaPagamento, double valor, double taxaKwh)
-    {
-        setNumeroFatura(numeroFatura);
-        setConsumoKwh(consumoKwh);
-        setDataVencimento(dataVencimento);
-        setFormaPagamento(formaPagamento);
-        setValor(valor);
-        setTaxaKwh(taxaKwh);
-    }
+	private long numeroFatura;
+	private double consumoKwh;
+	private String dataVencimento;
+	private String formaPagamento;
+	private double taxaKwh;
+	private double valor;
+	
+	public Fatura(long numeroFatura, double consumoKwh, String dataVencimento, String formaPagamento, double taxaKwh)
+	{
+		setNumeroFatura(numeroFatura);
+		setConsumoKwh(consumoKwh);
+		setDataVencimento(dataVencimento);
+		setFormaPagamento(formaPagamento);
+		setTaxaKwh(taxaKwh);
+		valor = taxaKwh * consumoKwh;
+		setValor(valor);
+	}
+	
+	public Fatura(){}
+	
+	public String getAll()
+	{
+		return "N�mero Fatura: "+numeroFatura+
+				"\nConsumo (KW/h): "+consumoKwh+
+				"\nData de vencimento: "+dataVencimento+
+				"\nForma de Pagamento: "+formaPagamento+
+				"\nTaxa (Reais por KW/h)"+taxaKwh+
+				"\nValor: R$ "+valor;
+	}
+	
+	public void setAll(long numeroFatura, double consumoKwh, String dataVencimento, String formaPagamento, double taxaKwh, double valor)
+	{
+		setNumeroFatura(numeroFatura);
+		setConsumoKwh(consumoKwh);
+		setDataVencimento(dataVencimento);
+		setFormaPagamento(formaPagamento);
+		setTaxaKwh(taxaKwh);
+		setValor(valor);
+	}
 
-    public int getNumeroFatura() {
-        return numeroFatura;
-    }
+	public long getNumeroFatura() {
+		return numeroFatura;
+	}
 
-    public void setNumeroFatura(int numeroFatura) {
-        this.numeroFatura = numeroFatura;
-    }
+	public void setNumeroFatura(long numeroFatura) {
+		this.numeroFatura = numeroFatura;
+	}
 
-    public double getConsumoKwh() {
-        return consumoKwh;
-    }
+	public double getConsumoKwh() {
+		return consumoKwh;
+	}
 
-    public void setConsumoKwh(double consumoKwh) {
-        this.consumoKwh = consumoKwh;
-    }
+	public void setConsumoKwh(double consumoKwh) {
+		this.consumoKwh = consumoKwh;
+	}
 
-    public String getDataVencimento() {
-        return dataVencimento;
-    }
+	public String getDataVencimento() {
+		return dataVencimento;
+	}
 
-    public void setDataVencimento(String dataVencimento) {
-        this.dataVencimento = dataVencimento;
-    }
+	public void setDataVencimento(String dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
 
-    public String getFormaPagamento() {
-        return formaPagamento;
-    }
+	public String getFormaPagamento() {
+		return formaPagamento;
+	}
 
-    public void setFormaPagamento(String formaPagamento) {
-        this.formaPagamento = formaPagamento;
-    }
+	public void setFormaPagamento(String formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
 
-    public double getValor() {
-        return valor;
-    }
+	public double getTaxaKwh() {
+		return taxaKwh;
+	}
 
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
+	public void setTaxaKwh(double taxaKwh) {
+		this.taxaKwh = taxaKwh;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
 
-    public double getTaxaKwh() {
-        return taxaKwh;
-    }
-
-    public void setTaxaKwh(double taxaKwh) {
-        this.taxaKwh = taxaKwh;
-    }
-    
-    
-    
-    
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
 }
 
 
